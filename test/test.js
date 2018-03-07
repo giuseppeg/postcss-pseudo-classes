eab2b4cf05ffc3c2999e1b28ea60f506a849b877
kevinkace <kevinacameron@hotmail.com>
2018-03-07 09:23:41 -0800

    Update dev dependencies and eslint config

--- test/test.js
+++ test/test.js
@@ -5,30 +5,30 @@ import test from 'ava';
 import plugin from '../';
 
 const read = fileName => readFileSync(fileName, 'utf-8');
-const inCSS = read('./fixtures/pseudos.css');
+const inCSS = read('./test/fixtures/pseudos.css');
 
 function run(t, input, output, opts = { }) {
   return postcss([ plugin(opts) ]).process(input.trim())
     .then( result => {
-      t.same(result.css, output.trim());
-      t.same(result.warnings().length, 0);
+      t.deepEqual(result.css, output.trim());
+      t.deepEqual(result.warnings().length, 0);
     });
 }
 
 test('should add proper pseudoclass selectors', t => {
-  const expectedOut = read('./fixtures/pseudos.out.css');
+  const expectedOut = read('./test/fixtures/pseudos.out.css');
   return run(t, inCSS, expectedOut, { });
 });
 
 test('should add all combinations (slower) of pseudoclass selectors ' +
   'if the `allCombinations` option is set to `true`', t => {
-  const expectedOut = read('./fixtures/pseudos-combinations.out.css');
+  const expectedOut = read('./test/fixtures/pseudos-combinations.out.css');
   return run(t, inCSS, expectedOut, { allCombinations: true });
 });
 
 test('should add pseudoclass selectors from a list and ignore the rest', t => {
-  const input = read('./fixtures/pseudos-restricted.css');
-  const expectedOut = read('./fixtures/pseudos-restricted.out.css');
+  const input = read('./test/fixtures/pseudos-restricted.css');
+  const expectedOut = read('./test/fixtures/pseudos-restricted.out.css');
   const restrictTo = [
     'nth-child',
     ':hover',
